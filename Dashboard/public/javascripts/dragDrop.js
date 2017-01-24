92d45c8bfbeedfcd833b0c7e653e06565dc8d752
Estudiante <carlos.leon.moran@gmail.com>
2017-01-24 17:20:49 -0500

    avance draggable

---
+++ Dashboard/public/javascripts/dragDrop.js
@@ -0,0 +1,47 @@
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '.draggable',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    $(event.target).css("backgroundColor","black");
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+    draggableElement.classList.add('can-drop');
+    draggableElement.textContent = 'Dragged in';
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+    event.relatedTarget.classList.remove('can-drop');
+    event.relatedTarget.textContent = 'Dragged out';
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped';
+    $(event.target).css("backgroundColor","black");
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
+});
+
+
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: false,
+    // keep the element within the area of it's parent
+    });
+
+    // enable autoS
