141e4865243590ac38a828927d436a250f5c4a9e
Carlos Leon <carlos.leon.moran@gmail.com>
2017-01-25 21:43:19 -0500

    Drag mejorado

--- Dashboard/public/javascripts/dragDrop.js
+++ Dashboard/public/javascripts/dragDrop.js
@@ -1,44 +1,3 @@
-interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: '.draggable',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
-
-  // listen for drop related events:
-
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    $(event.target).css("backgroundColor","black");
-  },
-  ondragenter: function (event) {
-    var draggableElement = event.relatedTarget,
-        dropzoneElement = event.target;
-
-    // feedback the possibility of a drop
-    dropzoneElement.classList.add('drop-target');
-    draggableElement.classList.add('can-drop');
-    draggableElement.textContent = 'Dragged in';
-  },
-  ondragleave: function (event) {
-    // remove the drop feedback style
-    event.target.classList.remove('drop-target');
-    event.relatedTarget.classList.remove('can-drop');
-    event.relatedTarget.textContent = 'Dragged out';
-  },
-  ondrop: function (event) {
-    event.relatedTarget.textContent = 'Dropped';
-    $(event.target).css("backgroundColor","black");
-  },
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active');
-    event.target.classList.remove('drop-target');
-  }
-});
-
-
-    // enable autoS
-
 interact('.draggable')
   .draggable({
     // enable inertial throwing
@@ -52,5 +11,73 @@ interact('.draggable')
     // enable autoScroll
     autoScroll: true,
 
-   
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(event.dx * event.dx +
+                     event.dy * event.dy)|0) + 'px');
+    }
   });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+
+//interact('.draggable').draggable(true);
+
+interact("#dropzone-des, #dropzone-fin").dropzone({
+  // only accept elements matching this CSS selector
+  accept: '.draggable',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 'pointer',
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+    $(dropzoneElement).css("backgroundColor","black");
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+    draggableElement.classList.add('can-drop');
+    
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+    $(event.target).css("backgroundColor","white");
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped';
+    console.log(event.target)
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+    console.log('No hace drop')
+  }
+});
