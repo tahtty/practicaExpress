92a6c7e4aeda535c4150c632d60c36be6e3fd2c0
tahtty <aahtty@espol.edu.ec>
2017-01-26 22:31:50 -0500

    funciones llenado al cambio de opción en el combo

--- Dashboard/public/javascripts/dashboard.js
+++ Dashboard/public/javascripts/dashboard.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
+  console.log('al menos hago esto')
     $("#combo").change(function(event) {
       var cambio=$(this).val();
-      iniciales(cambio);
-      enDesarrollo(cambio);
-      finalizado(cambio);
+      console.log(cambio);
+      inicialesc(cambio);
+      enDesarrolloc(cambio);
+      console.log('llego hasta aqui');
+      finalizadoc(cambio);
     });
 		agregarProyecto();
 		iniciales();
@@ -66,6 +69,7 @@ $(document).ready(function(){
     console.log('gotta gotta work.work,work,work... Gotta work!');
     var url = "#";//creo que se llamará a la función del modelo que tenga el query que saque de la base(?)
 
+//en el json sacar del proyecto 1
 //$.getJSON(url,{tipo: tareas ,format:"json"} ,function(resp) {
   for (var i =0 ; i<1 /*resp.length*/; i++) {
       var div = document.createElement("div");
@@ -89,9 +93,35 @@ $(document).ready(function(){
       $("#tini").append(div);
     }
 }//);}
+function inicialesc(c){
+  $("#tini").empty();
+  //$.getJSON(url,{tipo: tareas,proyecto: c ,format:"json"} ,function(resp) {//Este saca del proyecto especificado(se ha pasado el contenido html de la opción a la que se cambió)
+    for (var i =0 ; i<1 /*resp.length*/; i++) {
+      var div = document.createElement("div");
+      var pi=document.createElement("p");
+      var id=document.createElement("span");
+      $(id).html("5");//id de la tarea en la base
+      var sp=document.createElement("span");
+      var btd=document.createElement("button");
+      $(id).css('visibility', 'hidden');
+      $(div).addClass('draggable');
+      $(btd).addClass("btn btn-default");
+      $(btd).addClass("eliminar");
+      $(btd).attr('data-toggle', 'modal');
+      $(btd).attr('data-target', '#elimModal');
+      $(sp).addClass("glyphicon glyphicon-remove");
+      $(pi).html("Tarea 1 ");//resp[i].titulo
+      $(btd).append(sp);
+      $(div).append(pi);
+      $(div).append(btd);
+      $(div).append(id);
+      $("#tini").append(div);
+}
+}//);}
 	function enDesarrollo(){
     $("#tdes").empty();
     console.log('gotta gotta work.work,work,work... Gotta work!');
+    //en el json sacar del proyecto 1
   //$.getJSON(url,{tipo: tareas ,format:"json"} ,function(resp) {
   for (var i =0 ; i<1 /*resp.length*/; i++) {
 		 var div = document.createElement("div");
@@ -115,9 +145,75 @@ $(document).ready(function(){
       $("#tdes").append(div);
     }
 	}//);}
-	function finalizado(){
+  function enDesarrolloc(c){
+    $("#tdes").empty();
+    //$.getJSON(url,{tipo: tareas,proyecto: c ,format:"json"} ,function(resp) {Este saca del proyecto especificado(se ha pasado el contenido html de la opción a la que se cambió)
+      for (var i =0 ; i<1 /*resp.length*/; i++) {
+     var div = document.createElement("div");
+      var pi=document.createElement("p");
+      var id=document.createElement("span");
+      $(id).html("2");//id de la tarea
+      var sp=document.createElement("span");
+      var btd=document.createElement("button")
+      $(id).css('visibility', 'hidden');
+      $(div).addClass('draggable');
+      $(btd).addClass("btn btn-default");
+      $(btd).addClass('eliminar');
+      $(btd).attr('data-toggle', 'modal');
+      $(btd).attr('data-target', '#elimModal');
+      $(sp).addClass("glyphicon glyphicon-remove");
+      $(pi).html("Tarea 2 ");//resp[i].titulo
+       $(btd).append(sp);
+      $(div).append(pi);
+      $(div).append(btd);
+      $(div).append(id);
+      $("#tdes").append(div);
+    }
+  }//);}
+  function finalizado(){
+    console.log('entro1');
+    $("#tfin").empty();
+    //en el json sacar del proyecto 1
     //$.getJSON(url,{tipo: tareas ,format:"json"} ,function(resp) {
   for (var i =0 ; i<1 /*resp.length*/; i++) {
+    console.log('entro2');
+    var div = document.createElement("div");
+      var pi=document.createElement("p");
+      var id=document.createElement("span");
+      $(id).html("3");//id de la tarea
+      $(id).css('visibility', 'hidden');
+      $(div).addClass('draggable');
+      $(pi).html("Tarea 3 ");//resp[i].titulo
+      $(div).append(pi);
+      $(div).append(id);
+      $("#tfin").append(div);
+      console.log('finalizo');
+    }
+  }//);}
+   function finalizadoc(c){
+    $("#tfin").empty();
+    //$.getJSON(url,{tipo: tareas,proyecto: c ,format:"json"} ,function(resp) {Este saca del proyecto especificado(se ha pasado el contenido html de la opción a la que se cambió)
+      for (var i =0 ; i<1 /*resp.length*/; i++) {
+    var div = document.createElement("div");
+      var pi=document.createElement("p");
+      var id=document.createElement("span");
+      $(id).html("3");//id de la tarea
+      $(id).css('visibility', 'hidden');
+      $(div).addClass('draggable');
+      $(pi).html("Tarea 3 ");//resp[i].titulo
+      $(div).append(pi);
+      $(div).append(id);
+      $("#tfin").append(div);
+    }
+  }//);}
+
+	function finalizado(){
+    console.log('entro1');
+    $("#tfin").empty();
+    //en el json sacar del proyecto 1
+    //$.getJSON(url,{tipo: tareas ,format:"json"} ,function(resp) {
+  for (var i =0 ; i<1 /*resp.length*/; i++) {
+    console.log('entro2');
 		var div = document.createElement("div");
       var pi=document.createElement("p");
       var id=document.createElement("span");
@@ -128,5 +224,6 @@ $(document).ready(function(){
       $(div).append(pi);
       $(div).append(id);
       $("#tfin").append(div);
+      console.log('finalizo');
     }
 	}//);}
