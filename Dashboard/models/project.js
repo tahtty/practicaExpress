62999f3500161e57a509ad1963efb4b5def87fef
Julio Realpe <jrealpe94@gmail.com>
2017-01-27 00:36:42 -0500

    [ADD] user model

---
+++ Dashboard/models/project.js
@@ -0,0 +1,17 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ProjectSchema   = new Schema({
+    name:         {
+                    type:     String,
+                    required: true,
+                    trim:     true
+                  },
+    created_at:   {
+                    type:     Date,
+                    required: false,
+                    default:  Date.now
+                  }
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
