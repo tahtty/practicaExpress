b791fb7ebfbc1a93811d1baa777cde0e8feeb883
Julio Realpe <jrealpe94@gmail.com>
2017-01-27 00:37:13 -0500

    [ADD] CRUD of user model

--- Dashboard/routes/users.js
+++ Dashboard/routes/users.js
@@ -1,9 +1,63 @@
-var express = require('express');
-var router = express.Router();
+var mongoose = require('mongoose');
+var User  = mongoose.model('User');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//GET - Return all users in the DB
+exports.findAllUsers = function(req, res) {
+    User.find(function(err, users) {
+        if(err) res.send(500, err.message);
 
-module.exports = router;
+        console.log('GET /users')
+        res.status(200).jsonp(users);
+    });
+};
+
+
+//GET - Return a user with specified ID
+exports.findUserById = function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        if(err) return res.status(500).send(err.message);
+
+        console.log('GET /user/' + req.params.id);
+        res.status(200).jsonp(user);
+    });
+};
+
+
+//POST - Insert a new user in the DB
+exports.addUser = function(req, res) {
+    console.log('POST');
+    console.log(req.body);
+
+    var user = new User({
+        name: req.body.name
+    });
+
+    user.save(function(err, user) {
+        if(err) return res.status(500).send(err.message);
+        res.status(200).jsonp(user);
+    });
+};
+
+
+//PUT - Update a register already exists
+exports.updateUser = function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        user.name  = req.body.name;
+
+        user.save(function(err) {
+            if(err) return res.status(500).send(err.message);
+            res.status(200).jsonp(user);
+        });
+    });
+};
+
+
+//DELETE - Delete a User with specified ID
+exports.deleteUser = function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        user.remove(function(err) {
+            if(err) return res.status(500).send(err.message);
+            res.status(200).send();
+        })
+    });
+};
